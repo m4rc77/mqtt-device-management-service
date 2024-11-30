edce7073b7b032bb51f8540c193f9a716d8fdd91
Marc Schmid (mschmid) <mschmid@puzzle.ch>
2024-12-01 00:13:58 +0100

    Fixes to get chromium working under ubuntu

--- kiosk-start-chrome.sh
+++ kiosk-start-chrome.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# https://superuser.com/questions/461035/disable-google-chrome-session-restore-functionality
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' ~/.config/chromium/Default/Preferences
+DEFAULT_PREF="$HOME/.config/chromium/Default/Preferences"
+if [ -f "$DEFAULT_PREF" ]; then
+    echo "Patching: $DEFAULT_PREF ..."
+    # https://superuser.com/questions/461035/disable-google-chrome-session-restore-functionality
+    sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' "$DEFAULT_PREF"
+    sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' "$DEFAULT_PREF"
+fi
 
-chromium-browser --noerrors --disable-restore-session-state --disable-features=InfiniteSessionRestore --disable-session-crashed-bubble --disable-infobars --disable-translate --disable-sync --autoplay-policy=no-user-gesture-required --enable-kiosk-mode --kiosk "$@" > chrome.out 2>&1
+SNAP_PREF=$HOME/snap/chromium/common/chromium/Default/Preferences
+if [ -f "$SNAP_PREF" ]; then
+    echo "Patching: $SNAP_PREF ..."
+    # https://superuser.com/questions/461035/disable-google-chrome-session-restore-functionality
+    sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' "$SNAP_PREF"
+    sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' "$SNAP_PREF"
+fi
+
+# with the snap version of chromium the kiosk mode did not work
+# instead of calling chromium-browser use flatpack run ...
+flatpak run org.chromium.Chromium --noerrors  --noerrdialogs \
+                 --disable-restore-session-state --disable-features=InfiniteSessionRestore \
+                 --disable-session-crashed-bubble --disable-infobars --disable-translate --disable-sync \
+                 --no-first-run --start-maximized --disable-features=BookmarksBar \
+                 --autoplay-policy=no-user-gesture-required --enable-kiosk-mode --kiosk "$@" > chrome.out 2>&1
